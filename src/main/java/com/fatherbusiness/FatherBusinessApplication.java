a10d75c485f1178c6bd55e85d13e96e5a17bcd8f
Aman <amansingh1071999@gmail.com>
2023-07-30 16:45:13 +0530

    Initial commit

---
+++ src/main/java/com/fatherbusiness/FatherBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.fatherbusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FatherBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FatherBusinessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fatherbusiness/FatherBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatherbusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FatherBusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
